43f658104fe9c4ba3504b51d011363882d54288a
Derrick Manglallan <manglallan.derrick@gmail.com>
2024-01-15 13:27:17 +0800

    setting up basket class on going

---
+++ Core/Entities/CustometBasket.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class CustometBasket
+    {
+        public string Id { get; set; }
+        public List<BasketItem> Items { get; set; }
+        
+    }
+}
