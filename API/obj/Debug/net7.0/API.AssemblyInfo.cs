06bb133d2df32ef3d1b6c4dfaadd8f64040b678f
Derrick Manglallan <manglallan.derrick@gmail.com>
2024-03-01 20:06:28 +0800

    End of section 18

--- API/obj/Debug/net7.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net7.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d8db1b17336c2d3653946c169a1f3bc3ebae38e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+916c26746bf47dafc2e7091f6c940aa376602d61")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
